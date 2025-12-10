9fe727cb63b2bfe80f6eb36a31df3909f39ae977
ShorEnshan <esshor@163.com>
2025-12-10 20:41:29 +0800

    Learning Layers and LayerStack

---
+++ Hazel/src/hzpch.h
@@ -0,0 +1,18 @@
+#pragma once
+#define GLFW_STATIC
+#include <iostream>
+#include <functional>
+#include <memory>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+
+#include "Hazel/Log.h"
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
